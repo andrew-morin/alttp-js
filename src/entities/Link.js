208ef8b9ca54a58f453d3842e6382235a0f07c9b
Andrew Morin <andrew.morin@appian.com>
2019-08-06 20:11:40 -0400

    Some refactoring of direction calculations

--- src/entities/Link.js
+++ src/entities/Link.js
@@ -35,6 +35,7 @@ export function getLink() {
   link.animationSpeed = 1/2;
   link.play();
 
+  // TODO: Create separate Link class that extends AnimatedSprite
   link.xSub = 0;
   link.ySub = 0;
   link.update = updateLink;
@@ -50,47 +51,26 @@ export function getLink() {
 
 function updateLink(keyboard, window, background) {
   const { left, right, up, down, directionChange } = keyboard;
-  const { state: { action, direction }} = link;
+  const { state } = link;
   const vx = left.pressed ? -1 : right.pressed ? 1 : 0;
   const vy = up.pressed ? -1 : down.pressed ? 1 : 0;
   const moving = vx !== 0 || vy !== 0;
-  let newAction = moving ? Actions.WALK : Actions.STAND;
-  let newDirection = direction;
+  const newAction = moving ? Actions.WALK : Actions.STAND;
+  const newDirection = getNewDirection(directionChange, vx, vy, state.direction);
+  const newState = {
+    action: newAction,
+    direction: newDirection
+  };
 
-  // TODO: Factor out to helper function
-  // TODO: Factor out cases 1 and -1 to separate helper function
-  if (directionChange && moving) {
-    switch (vx) {
-      case 0:
-        newDirection = vy === 1 ? Directions.DOWN : Directions.UP;
-        break;
-      case 1:
-        if (vy === 0 || direction === Directions.RIGHT) {
-          newDirection = Directions.RIGHT
-        }
-        if (vy === 1 && Directions.DOWN !== direction || vy === -1 && Directions.UP !== direction) {
-          newDirection = Directions.RIGHT
-        }
-        break;
-      case -1:
-        if (vy === 0 || direction === Directions.LEFT) {
-          newDirection = Directions.LEFT
-        }
-        if (vy === 1 && Directions.DOWN !== direction || vy === -1 && Directions.UP !== direction) {
-          newDirection = Directions.LEFT
-        }
-        break;
-    }
-  }
-
-  // Reset subpixels and set new textures when direction has changed or action changed
-  if (newDirection !== direction || newAction !== action) {
+  // Reset subpixels when direction has changed
+  if (directionChange) {
     link.xSub = 0;
     link.ySub = 0;
-    link.state = {
-      action: newAction,
-      direction: newDirection
-    };
+  }
+
+  // Update textures if Link has changed direction or action
+  if (hasLinkStateChanged(state, newState)) {
+    link.state = newState;
     if (newAction === Actions.WALK) {
       // TODO: rename textures to snake case, e.g. walk_right
       link.textures = link.spritesheet.animations[Actions.WALK + newDirection];
@@ -100,12 +80,41 @@ function updateLink(keyboard, window, background) {
   }
 
   // Only update link if he's moving
-  if (vx !== 0 || vy !== 0) {
+  if (moving) {
     const movement = vx === 0 || vy === 0 ? cardinal : diagonal;
 
     updatePosition(link, movement, vx, vy);
     contain(link, window);
   }
+}
+
+function hasLinkStateChanged(oldState, newState) {
+  return oldState.action !== newState.action || oldState.direction !== newState.direction;
+}
+
+function getNewDirection(directionChange, vx, vy, direction) {
+  if (directionChange && (vx !== 0 || vy !== 0)) {
+    switch (vx) {
+      case 0:
+        return vy === 1 ? Directions.DOWN : Directions.UP;
+      case 1:
+        if (shouldUpdateHorizDirection(vy, direction)) {
+          return Directions.RIGHT
+        }
+        break;
+      case -1:
+        if (shouldUpdateHorizDirection(vy, direction)) {
+          return Directions.LEFT
+        }
+        break;
+    }
+  }
+
+  return direction;
+}
+
+function shouldUpdateHorizDirection(vy, direction) {
+  return vy === 0 || vy === 1 && Directions.DOWN !== direction || vy === -1 && Directions.UP !== direction;
 }
 
 function updatePosition(link, movement, vx, vy) {
