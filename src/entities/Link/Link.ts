ed348eadff41876f717625db9ab1fa402e1e6b44
Andrew Morin <andrew.morin@appian.com>
2020-10-05 17:17:04 -0400

    Change pointsToCheck to us PIXI.Point

--- src/entities/Link/Link.ts
+++ src/entities/Link/Link.ts
@@ -1,6 +1,6 @@
-import {AnimatedSprite, Container, Loader, Spritesheet} from 'pixi.js';
+import { AnimatedSprite, Container, Loader, Spritesheet } from 'pixi.js';
 import Tile from '../../tiles/Tile';
-import {Keyboard} from '../../index';
+import { Keyboard } from '../../index';
 import { detectCollisions } from './collisionDetection';
 
 enum Direction {
@@ -34,7 +34,7 @@ export class Link extends AnimatedSprite {
     this.startDoorTransition = startDoorTransition;
 
     // AnimatedSprite fields
-    this.animationSpeed = 1/2;
+    this.animationSpeed = 1 / 2;
     this.updateAnchor = true;
 
     // Link fields
@@ -81,14 +81,14 @@ export class Link extends AnimatedSprite {
     if (moving) {
       let cardinal = 0;
       let diagonal = 0;
-      const standingTile = background.children.find(tile => {
+      const standingTile = background.children.find((tile) => {
         if (tile instanceof Tile) {
           return tile.containsPoint(this.getGlobalPosition());
         }
         return false;
       });
       if (standingTile instanceof Tile) {
-        ({cardinal, diagonal} = standingTile.linkMovement);
+        ({ cardinal, diagonal } = standingTile.linkMovement);
       }
       const movement = vx === 0 || vy === 0 ? cardinal : diagonal;
 
@@ -102,7 +102,7 @@ export class Link extends AnimatedSprite {
   }
 
   shouldUpdateHorizDirection(vy: number, direction: Direction): boolean {
-    return vy === 0 || vy === 1 && Direction.DOWN !== direction || vy === -1 && Direction.UP !== direction;
+    return vy === 0 || (vy === 1 && Direction.DOWN !== direction) || (vy === -1 && Direction.UP !== direction);
   }
 
   getNewDirection(directionChange: boolean, vx: number, vy: number, direction: Direction): Direction {
@@ -146,6 +146,98 @@ export class Link extends AnimatedSprite {
     this.xSub = xSub;
     this.y = newY;
     this.ySub = ySub;
+  }
+
+  detectCollisions(vx: number, vy: number, background: Container): [number, number] {
+    const linkRow = Math.floor(this.y / 16);
+    const rowsToCheck = [linkRow - 1, linkRow, linkRow + 1];
+    const linkCol = Math.floor(this.x / 16);
+    const colsToCheck = [linkCol - 1, linkCol, linkCol + 1];
+    const tilesPerRow = Math.floor(background.width / 16);
+    const tilesToCheck: DisplayObject[] = [];
+    rowsToCheck.forEach((row) => {
+      colsToCheck.forEach((col) => {
+        const index = row * tilesPerRow + col;
+        if (index >= 0 && index < background.children.length) {
+          tilesToCheck.push(background.getChildAt(index));
+        }
+      });
+    });
+    let newX = this.x + vx,
+      newY = this.y + vy;
+    let xPointsToCheck: Point[] | undefined;
+    if (vx > 0) {
+      xPointsToCheck = [new Point(newX + 8, this.y + 7), new Point(newX + 8, this.y), new Point(newX + 8, this.y - 8)];
+    } else if (vx < 0) {
+      xPointsToCheck = [new Point(newX - 8, this.y + 7), new Point(newX - 8, this.y), new Point(newX - 8, this.y - 8)];
+    }
+    if (xPointsToCheck) {
+      const [collidedTile, collidedPoints] = this.detectCollisionHelper(xPointsToCheck, tilesToCheck);
+      if (collidedTile) {
+        const unitVelocity = vx / Math.abs(vx);
+        const collisionShape = collidedTile.collisionShape as Rectangle;
+        const halfWidth = collisionShape.width / 2;
+        const centerOfTile = collidedTile.x + collisionShape.x + halfWidth;
+        newX = centerOfTile + -unitVelocity * (halfWidth + 8);
+      }
+
+      if (vy === 0 && !collidedPoints[1]) {
+        if (collidedPoints[0]) {
+          newY -= 1;
+        } else if (collidedPoints[2]) {
+          newY += 1;
+        }
+      }
+    }
+    let yPointsToCheck: Point[] | undefined;
+    if (vy > 0) {
+      yPointsToCheck = [new Point(newX + 7, newY + 8), new Point(newX, newY + 8), new Point(newX - 8, newY + 8)];
+    } else if (vy < 0) {
+      yPointsToCheck = [new Point(newX + 7, newY - 8), new Point(newX, newY - 8), new Point(newX - 8, newY - 8)];
+    }
+    if (yPointsToCheck) {
+      const [collidedTile, collidedPoints] = this.detectCollisionHelper(yPointsToCheck, tilesToCheck);
+      if (collidedTile) {
+        const unitVelocity = vy / Math.abs(vy);
+        const collisionShape = collidedTile.collisionShape as Rectangle;
+        const halfHeight = collisionShape.height / 2;
+        const centerOfTile = collidedTile.y + collisionShape.y + halfHeight;
+        newY = centerOfTile + -unitVelocity * (halfHeight + 8);
+
+        if (vx === 0 && !collidedPoints[1]) {
+          if (collidedPoints[0]) {
+            newX -= 1;
+          } else if (collidedPoints[2]) {
+            newX += 1;
+          }
+        }
+      }
+    }
+
+    return [newX, newY];
+  }
+
+  detectCollisionHelper(pointsToCheck: Point[], tilesToCheck: DisplayObject[]): [Tile | undefined, boolean[]] {
+    const collidedPoints = new Array(pointsToCheck.length).fill(false);
+    let collidedTile;
+    tilesToCheck.forEach((child) => {
+      const tile = child as Tile;
+      if (tile.x > this.x + 9 || tile.x < this.x - 25 || tile.y > this.y + 9 || tile.y < this.y - 25) {
+        return;
+      }
+      pointsToCheck.forEach((point, index) => {
+        const localX = point.x - tile.x;
+        const localY = point.y - tile.y;
+        if (tile.collidesWithPoint(localX, localY)) {
+          tile.updateOnCollision(this.startDoorTransition);
+          collidedPoints[index] = true;
+          collidedTile = tile;
+        } else if (tile.containsPoint(point)) {
+          tile.updateOnOverlap(localX, localY);
+        }
+      });
+    });
+    return [collidedTile, collidedPoints];
   }
 
   contain(background: Container): void {
