41bb9fb55bc137dce0805d68db5d4d8a7dd60161
andrew-morin <andrew.morin@appian.com>
2022-01-20 05:33:11 +0000

    Deploying to gh-pages from @ andrew-morin/alttp-js@07ea666874ee6f0dfea049a20577dcda5d284c12 🚀

--- src/tiles/BackgroundColorTile.ts
+++
@@ -1,8 +0,0 @@
-import { Texture } from "pixi.js";
-import Tile from "./Tile";
-
-export default function backgroundColorTile(color: number): Tile {
-  const tile = new Tile(Texture.WHITE, { solid: true });
-  tile.tint = color;
-  return tile;
-}
